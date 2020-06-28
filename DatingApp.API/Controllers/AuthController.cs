d273c372ab2b06d5980ae3083303ce46ff3d372e
karem_1996 <kokookoko1996@gmail.com>
2020-06-28 03:10:11 +0200

    Commit

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,11 +1,41 @@
+using System.Threading.Tasks;
+using DatingApp.API.Controllers.Models;
+using DatingApp.API.Controllers.Models.Data;
+using DatingApp.API.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
 namespace DatingApp.API.Controllers
 {
-
     [Route("api/[controller]")]
     [ApiController]
 
-    public class AuthController
+    public class AuthController : ControllerBase
     {
-        
+        private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            // validate request
+
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+
+            if (await _repo.UserExists(userForRegisterDto.Username))
+                return BadRequest("Username already exists");
+
+            var userToCreate = new User
+            {
+                Username = userForRegisterDto.Username
+            };
+
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+
+            return StatusCode(201);
+        }
     }
 }
